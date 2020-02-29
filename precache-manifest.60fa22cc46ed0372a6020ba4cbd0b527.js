066cedd43fa64c31ea170eee0438823c2562368b
cardosodiaseduardo <cardosodiaseduardo@gmail.com>
2020-02-29 10:22:02 -0400

    Att em 29/02/2020 as 10:21

---
+++ precache-manifest.60fa22cc46ed0372a6020ba4cbd0b527.js
@@ -0,0 +1,270 @@
+self.__precacheManifest = [
+  {
+    "revision": "8dd829556ec73c4c2d24",
+    "url": "/jakagame/css/app.b9c16671.css"
+  },
+  {
+    "revision": "8dd829556ec73c4c2d24",
+    "url": "/jakagame/js/app.cc6019df.js"
+  },
+  {
+    "revision": "4e0c8fdd5c495761a036",
+    "url": "/jakagame/css/chunk-vendors.c0a6802b.css"
+  },
+  {
+    "revision": "4e0c8fdd5c495761a036",
+    "url": "/jakagame/js/chunk-vendors.d5cd11c1.js"
+  },
+  {
+    "revision": "a6dc328c42abb0c38d156e544c281908",
+    "url": "/jakagame/img/tresdepaus.a6dc328c.png"
+  },
+  {
+    "revision": "c09127539b4764e1644c7a11feb6f8ba",
+    "url": "/jakagame/img/setedeouros.c0912753.png"
+  },
+  {
+    "revision": "12724dea58420b01b17f40b0d7141847",
+    "url": "/jakagame/img/valetedecopas.12724dea.png"
+  },
+  {
+    "revision": "ecd63b99a6e12fa252b38e29531d9a92",
+    "url": "/jakagame/img/reisdecopas.ecd63b99.png"
+  },
+  {
+    "revision": "41284290cf2dcc5e3ff06c93e27651ba",
+    "url": "/jakagame/img/doisdecopas.41284290.png"
+  },
+  {
+    "revision": "8781dd22653eccc80be85a14a98e012f",
+    "url": "/jakagame/img/cincodepaus.8781dd22.png"
+  },
+  {
+    "revision": "0183b2793712ccd6f986d57870f571a7",
+    "url": "/jakagame/img/reisdeespadas.0183b279.png"
+  },
+  {
+    "revision": "368d54544c99e395235bae22aff60cc3",
+    "url": "/jakagame/img/doisdeouros.368d5454.png"
+  },
+  {
+    "revision": "5fbca5c0467dfc4ab6481ee10f0fa98d",
+    "url": "/jakagame/img/quatrodeespadas.5fbca5c0.png"
+  },
+  {
+    "revision": "9dcf2dc9c6fc355a4eefe15bb5177404",
+    "url": "/jakagame/img/cincodeouros.9dcf2dc9.png"
+  },
+  {
+    "revision": "361d3da8b6d9b5d0bef7065a52ab178e",
+    "url": "/jakagame/img/tresdecopas.361d3da8.png"
+  },
+  {
+    "revision": "baf91ac8b1061e48bfa44f732290f4e7",
+    "url": "/jakagame/img/damadeespadas.baf91ac8.png"
+  },
+  {
+    "revision": "951cd00a1f1bf6d51d5e859a95dc2c30",
+    "url": "/jakagame/img/novedepaus.951cd00a.png"
+  },
+  {
+    "revision": "92fc8ac163198f4a1c75a2cc17cd701e",
+    "url": "/jakagame/img/setedecopas.92fc8ac1.png"
+  },
+  {
+    "revision": "e0e56209759b5051ca9a751f77e9a257",
+    "url": "/jakagame/img/asdeouros.e0e56209.png"
+  },
+  {
+    "revision": "f5b92a6e121c98c6f14e82046b28e59e",
+    "url": "/jakagame/img/dezdecopas.f5b92a6e.png"
+  },
+  {
+    "revision": "86cdf7ee66c6302c34adb8561f9f9a1a",
+    "url": "/jakagame/img/dezdepaus.86cdf7ee.png"
+  },
+  {
+    "revision": "f3c512dcc29d2737556259952690fae4",
+    "url": "/jakagame/img/blackcoringa.f3c512dc.png"
+  },
+  {
+    "revision": "f24b28b39b51e442502e421dcc7c427e",
+    "url": "/jakagame/img/seisdepaus.f24b28b3.png"
+  },
+  {
+    "revision": "79aa6a88d3776b7b61f30436953a6712",
+    "url": "/jakagame/img/oitodecopas.79aa6a88.png"
+  },
+  {
+    "revision": "bb0470c9d8f818c5ea5cfcd0a3072e44",
+    "url": "/jakagame/img/cartaVirada4.bb0470c9.png"
+  },
+  {
+    "revision": "a2733c2e3396153e597ead2b558e2af5",
+    "url": "/jakagame/img/brazilsflag.a2733c2e.svg"
+  },
+  {
+    "revision": "3dc83db779003bd4fae2593a2f9892a8",
+    "url": "/jakagame/img/cincodecopas.3dc83db7.png"
+  },
+  {
+    "revision": "9ddde0c866ffd16ff8756a585870509d",
+    "url": "/jakagame/img/oitodeespadas.9ddde0c8.png"
+  },
+  {
+    "revision": "dfed6e25cd7a86c01e344facd5fc7818",
+    "url": "/jakagame/img/reisdepaus.dfed6e25.png"
+  },
+  {
+    "revision": "16e33d9f400bfa87b33bc6963f48b662",
+    "url": "/jakagame/img/novedeespadas.16e33d9f.png"
+  },
+  {
+    "revision": "bb0470c9d8f818c5ea5cfcd0a3072e44",
+    "url": "/jakagame/img/cartaVirada3.bb0470c9.png"
+  },
+  {
+    "revision": "8cfc0cba7180603dbcdc1846b84b2f6d",
+    "url": "/jakagame/img/seisdecopas.8cfc0cba.png"
+  },
+  {
+    "revision": "fd888f4dbb0aa7098ac84b41b7fb2080",
+    "url": "/jakagame/img/seisdeespadas.fd888f4d.png"
+  },
+  {
+    "revision": "f3c512dcc29d2737556259952690fae4",
+    "url": "/jakagame/img/redcoringa.f3c512dc.png"
+  },
+  {
+    "revision": "bb0470c9d8f818c5ea5cfcd0a3072e44",
+    "url": "/jakagame/img/cartaVirada.bb0470c9.png"
+  },
+  {
+    "revision": "ed2f2904f39ca15104cb29fa14dde3a6",
+    "url": "/jakagame/img/asdecopas.ed2f2904.png"
+  },
+  {
+    "revision": "8b0b07b4e2a9c1dbbcfb14ec2e6c2d72",
+    "url": "/jakagame/img/tresdeouros.8b0b07b4.png"
+  },
+  {
+    "revision": "e5027b11ea0095b59137bcac55df9638",
+    "url": "/jakagame/img/oitodeouros.e5027b11.png"
+  },
+  {
+    "revision": "9350254948e6e3ee035527298e71b4b4",
+    "url": "/jakagame/img/asdeespadas.93502549.png"
+  },
+  {
+    "revision": "e1a26740f156d95ea6a15f563c0b5bfb",
+    "url": "/jakagame/img/damadepaus.e1a26740.png"
+  },
+  {
+    "revision": "91c0c02249798976fc5a3281327db663",
+    "url": "/jakagame/img/quatrodecopas.91c0c022.png"
+  },
+  {
+    "revision": "6bf0a144780645ef97c984ad8ecc6b69",
+    "url": "/jakagame/img/usaflag.6bf0a144.svg"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/jakagame/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "22b96525970ec3a8cf8756717bdb447a",
+    "url": "/jakagame/img/doisdepaus.22b96525.png"
+  },
+  {
+    "revision": "ebc8b47137db373261ccb5115af3a00c",
+    "url": "/jakagame/img/valetedeouros.ebc8b471.png"
+  },
+  {
+    "revision": "7da136608b8785b01809b56b74975383",
+    "url": "/jakagame/img/setedepaus.7da13660.png"
+  },
+  {
+    "revision": "de4805050f798ddf54922e95015f75cf",
+    "url": "/jakagame/img/reisdeouros.de480505.png"
+  },
+  {
+    "revision": "4e03f8ee36cf04d186424d3669cf9cf1",
+    "url": "/jakagame/img/valetedeespadas.4e03f8ee.png"
+  },
+  {
+    "revision": "735830ec2a9767e25a7c271393d6f26e",
+    "url": "/jakagame/img/damadeouros.735830ec.png"
+  },
+  {
+    "revision": "ecf4fc16e09df4dee8b15c50168a3541",
+    "url": "/jakagame/img/dezdeespadas.ecf4fc16.png"
+  },
+  {
+    "revision": "cf56252f1dc0769d96e768e184ffca92",
+    "url": "/jakagame/img/oitodepaus.cf56252f.png"
+  },
+  {
+    "revision": "b8114c2227d1ec6b20f5e355c2d39421",
+    "url": "/jakagame/img/damadecopas.b8114c22.png"
+  },
+  {
+    "revision": "7dbe5779a07ba0c153224483651c2962",
+    "url": "/jakagame/img/dezdeouros.7dbe5779.png"
+  },
+  {
+    "revision": "31a121ff37b23e9c6a8c28ea6f09b12e",
+    "url": "/jakagame/img/quatrodepaus.31a121ff.png"
+  },
+  {
+    "revision": "51a6acd08e685e92f8075109a73ba126",
+    "url": "/jakagame/img/cincodeespadas.51a6acd0.png"
+  },
+  {
+    "revision": "3c7d1997f93fa441cb27dcc83055608e",
+    "url": "/jakagame/img/seisdeouros.3c7d1997.png"
+  },
+  {
+    "revision": "c6fea981b783e6748791d9b1f66a58db",
+    "url": "/jakagame/img/asdepaus.c6fea981.png"
+  },
+  {
+    "revision": "5a73642b26461a8db49e8cb9dbd33732",
+    "url": "/jakagame/img/novedecopas.5a73642b.png"
+  },
+  {
+    "revision": "bb0470c9d8f818c5ea5cfcd0a3072e44",
+    "url": "/jakagame/img/cartaVirada2.bb0470c9.png"
+  },
+  {
+    "revision": "8239a55e9dcaa48f291956355118f76d",
+    "url": "/jakagame/img/tresdeespadas.8239a55e.png"
+  },
+  {
+    "revision": "6abc59e8c92edafbd44f492ddbab6015",
+    "url": "/jakagame/img/doisdeespadas.6abc59e8.png"
+  },
+  {
+    "revision": "71c2dacadbb34eb5dcf5183b390d4790",
+    "url": "/jakagame/img/valetedepaus.71c2daca.png"
+  },
+  {
+    "revision": "e807280a90a23f5a25c23d7219efda6c",
+    "url": "/jakagame/img/novedeouros.e807280a.png"
+  },
+  {
+    "revision": "7b5d0016c44f94ff2b4e994a4d4b94f5",
+    "url": "/jakagame/img/quatrodeouros.7b5d0016.png"
+  },
+  {
+    "revision": "9314d91cdd752608f522377a6afa2224",
+    "url": "/jakagame/img/setedeespadas.9314d91c.png"
+  },
+  {
+    "revision": "2fde3adace26619c02b98f929349941d",
+    "url": "/jakagame/index.html"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/jakagame/robots.txt"
+  }
+];
